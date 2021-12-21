421c9ad2ee8d7374ee4b273996d92be4aacf6a8c
Tosin Aremu <66263731+Tohsyne@users.noreply.github.com>
2021-12-21 13:50:25 +0100

    added comments

--- src/server.js
+++ src/server.js
@@ -7,9 +7,35 @@ export const app = express()
 
 app.disable('x-powered-by')
 
+//global middlewares
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+ 
+//middleware 
+const dontLog = (req, res, next) =>{
+    console.log ('Not logging')
+    next()
+}
 
-export const start = () => {}
+const log = (req, res, next) =>{
+    console.log ('logging')
+    next()
+}
+//controller function including the middleware
+app.get('/users', [dontLog, log, dontLog], (req, res) => {
+ res.send({message: 'hello', person: "tosin" })
+})
+//controller function exclduing middleware
+app.post('/', (req, res)=>{
+  console.log(req.body)
+  res.send({message: 'ok'})
+})
+
+//function to start server
+export const start = () => {
+    app.listen(3000, ()=>{
+        console.log('server is running on 3000')
+    })
+}
